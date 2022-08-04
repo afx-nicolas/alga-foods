7f7795085c9ba4063139a927704d190f87f789ef
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-08-04 12:37:41 -0300

    build: add sass configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src')],
+    prependData: `@import "styles/variables.sass"`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
