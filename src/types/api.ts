79062883dc5128c1736756f218d92534ea33f38f
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-08-09 13:26:14 -0300

    add get restaurant type

--- src/types/api.ts
+++ src/types/api.ts
@@ -24,3 +24,38 @@ export interface GetRestaurantsType {
     };
   };
 }
+
+export interface GetRestaurantType {
+  aberto: boolean;
+  ativo: boolean;
+  cozinha: {
+    id: number;
+    nome: string;
+  };
+  endereco: {
+    bairro: string;
+    cep: string;
+    cidade: {
+      estado: string;
+      id: number;
+      nome: string;
+    };
+    logradouro: string;
+  };
+  nome: string;
+  taxaFrete: number;
+}
+
+export interface ProdutoType {
+  ativo: boolean;
+  descricao: string;
+  id: number;
+  nome: string;
+  preco: number;
+}
+
+export interface GetProdutosType {
+  _embedded: {
+    produtos: ProdutoType[];
+  };
+}
