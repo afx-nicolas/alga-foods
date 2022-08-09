92d5dcf2f30ad77cf3ef013bb35fa6d0367c18c7
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-08-09 14:57:45 -0300

    feat: add loading animation to nav

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,19 +1,27 @@
+import { useLoading } from '../../hooks';
+import styles from './Layout.module.sass';
+
 import Footer from '../Footer';
 import Header from '../Header';
-import styles from './Layout.module.sass';
+import Loading from '../Loading';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const [isLoading, handleSetLoading] = useLoading();
+
   return (
-    <div className={styles.layout}>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.container}>{children}</div>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div className={styles.layout}>
+        <Header handleSetLoading={handleSetLoading} />
+        <main className={styles.main}>
+          <div className={styles.container}>{children}</div>
+        </main>
+        <Footer />
+      </div>
+      {isLoading && <Loading />}
+    </>
   );
 }
