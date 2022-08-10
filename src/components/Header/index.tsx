49702ead532cb6179810f3f90da438b839f3b7a5
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-08-10 12:47:23 -0300

    feat: add responsive mobile menu and nav links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,41 +4,24 @@ import styles from './Header.module.sass';
 
 import Logo from '../Logo';
 import Cart from '../Cart';
-import { useRouter } from 'next/router';
+import MobileMenuButton from '../MobileMenuButton';
+import NavLinks from '../NavLinks';
 
-interface HeaderProps {
-  handleSetLoading: () => void;
-}
-
-export default function Header({ handleSetLoading }: HeaderProps) {
-  const router = useRouter();
-
-  const isHomePage = router.pathname === '/';
-  const isRestaurantesPage = router.pathname === '/restaurantes';
-
+export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Link href="/">
-          <a onClick={isHomePage ? () => {} : handleSetLoading}>
+          <a>
             <Logo />
           </a>
         </Link>
         <section className={styles.actions}>
-          <nav className={styles.nav}>
-            <ul>
-              <Link href="/">
-                <a onClick={isHomePage ? () => {} : handleSetLoading}>
-                  <li>Inicio</li>
-                </a>
-              </Link>
-              <Link href="/restaurantes">
-                <a onClick={isRestaurantesPage ? () => {} : handleSetLoading}>
-                  <li>Restaurantes</li>
-                </a>
-              </Link>
-            </ul>
-          </nav>
+          <MobileMenuButton>
+            <nav className={styles.nav}>
+              <NavLinks />
+            </nav>
+          </MobileMenuButton>
           <Cart />
         </section>
       </div>
