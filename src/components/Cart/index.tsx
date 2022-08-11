edba47e0ec9821143717a16c6873bbad367595a8
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-08-11 13:03:16 -0300

    feat: add dynamic cart badge products display

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,13 +1,22 @@
+import { useContext } from 'react';
 import { ShoppingCart } from 'phosphor-react';
+import { CartContext } from '../../contexts/CartContext';
 
 import styles from './Cart.module.sass';
 
 export default function Cart() {
+  const { cart } = useContext(CartContext);
+  const cartItems = Object.keys(cart).length;
+
   return (
     <button className={styles.cart}>
       <div>
         <ShoppingCart size={24} />
-        <span className={styles.badge}>+9</span>
+        {cartItems && (
+          <span className={styles.badge}>
+            {cartItems > 9 ? '+9' : cartItems}
+          </span>
+        )}
       </div>
     </button>
   );
