de39d774619d9c539760e2f7d1f7b72aa507c459
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-08-11 13:05:11 -0300

    feat: add cart link

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -3,21 +3,24 @@ import { ShoppingCart } from 'phosphor-react';
 import { CartContext } from '../../contexts/CartContext';
 
 import styles from './Cart.module.sass';
+import Link from 'next/link';
 
 export default function Cart() {
   const { cart } = useContext(CartContext);
   const cartItems = Object.keys(cart).length;
 
   return (
-    <button className={styles.cart}>
-      <div>
-        <ShoppingCart size={24} />
-        {cartItems && (
-          <span className={styles.badge}>
-            {cartItems > 9 ? '+9' : cartItems}
-          </span>
-        )}
-      </div>
-    </button>
+    <Link href="/carrinho">
+      <a className={styles.cart}>
+        <div>
+          <ShoppingCart size={24} />
+          {!!cartItems && (
+            <span className={styles.badge}>
+              {cartItems > 9 ? '+9' : cartItems}
+            </span>
+          )}
+        </div>
+      </a>
+    </Link>
   );
 }
