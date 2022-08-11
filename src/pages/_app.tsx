ff5dc1a47ddf3df5cb46a0bb1f8bcad2d800f329
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-08-11 12:53:21 -0300

    feat: use cart context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from 'next/app';
 
 import Layout from '../components/Layout';
 import MobileMenuProvider from '../contexts/MobileMenuContext';
+import CartContextProvider from '../contexts/CartContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MobileMenuProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </MobileMenuProvider>
+    <CartContextProvider>
+      <MobileMenuProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MobileMenuProvider>
+    </CartContextProvider>
   );
 }
 
